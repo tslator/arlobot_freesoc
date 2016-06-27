a0e9168400a43671cf7b28ca8cddf94e75ebce40
tslator <theslators@gmail.com>
2016-06-26 19:59:17 -0700

    update

--- freesoc.cydsn/codegentemp/freesoc.v
+++ freesoc.cydsn/codegentemp/freesoc.v
@@ -1,6 +1,6 @@
 // ======================================================================
 // freesoc.v generated from TopDesign.cysch
-// 06/26/2016 at 07:38
+// 06/26/2016 at 19:52
 // This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
 // ======================================================================
 
--- freesoc.cydsn/codegentemp/freesoc_u.sdc
+++ freesoc.cydsn/codegentemp/freesoc_u.sdc
@@ -1,3 +1,3 @@
 # Component constraints for D:\projects\ArloBot\arlobot_freesoc\freesoc.cydsn\TopDesign\TopDesign.cysch
 # Project: D:\projects\ArloBot\arlobot_freesoc\freesoc.cydsn\freesoc.cyprj
-# Date: Sun, 26 Jun 2016 14:38:33 GMT
+# Date: Mon, 27 Jun 2016 02:52:25 GMT
